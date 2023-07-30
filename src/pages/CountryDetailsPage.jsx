6ecd837929978589e61bf10dca0abb5461d7a170
Andrej Delinac <andrejdelinac@gmail.com>
2023-07-30 10:09:03 +0200

    solved lab

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const CountryDetails = () => {
-  const [fetching, setFetching] = useState(true);
-  const [details, setDetails] = useState({});
+  // const [fetching, setFetching] = useState(true);
+  const [details, setDetails] = useState(null);
   const { countryId } = useParams();
   useEffect(() => {
     const fetchData = async () => {
@@ -15,18 +15,35 @@ const CountryDetails = () => {
 
         //   console.log(response.data);
         setDetails(response.data);
-        setFetching(false);
-        console.log("THIS IS GOOD NIOW YAY: ", details);
+        // setFetching(false);
+        // console.log("THIS IS GOOD NIOW YAY: ", details);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, [countryId]);
+  // console.log("BABA", details);
+
+  if (details === null || Object.keys(details).length === 0) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
       <h1>Country Details</h1>
+      <h3>Name: {details.name.common}</h3>
+      <p>Capital: {details.capital[0]}</p>
+      <p>Area: {details.area}</p>
+      <ul>
+        {details.borders.map((border) => {
+          return (
+            <li key={border}>
+              <Link to={`/${border}`}>{border}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
